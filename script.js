3105fe628eaa3e7f531a6bf9940e56255e840860
Senthamilan-07032003 <thamilansen2003@gmail.com>
2024-07-10 14:33:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var a=prompt("Ethachum enter panni thola:");
+document.getElementById("js").innerHTML=a;
+window.alert("website hackapattathu");
+alert("nakku");
+console.log("hello confidential  content");
